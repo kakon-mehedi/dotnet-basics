9c7ca21bb813a609244181434e9faca3c4f8236c
kakon-selise <kakon.mehedi@selise.ch>
2024-03-15 14:37:35 +0600

    feat: Add dev branch

--- src/dontnet-fundamentals-kakon/Program.cs
+++ src/dontnet-fundamentals-kakon/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World First!");
+app.MapGet("/", () => "Hello World for Dev!");
 
 app.Run();
