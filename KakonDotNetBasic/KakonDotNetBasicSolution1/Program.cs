e09c25ad0a277e5695ce0c44f8f1b593b11b3d8e
kakon-selise <kakon.mehedi@selise.ch>
2024-04-11 11:50:12 +0600

    init

--- KakonDotNetBasic/KakonDotNetBasicSolution1/Program.cs
+++ KakonDotNetBasic/KakonDotNetBasicSolution1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World From Dev!");
+app.MapGet("/", () => "Hello World From Routing!");
 
 app.Run();
